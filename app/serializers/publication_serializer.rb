db483eed4af8a3a7d41a51f5afd7ab546ed3c0a4
Tyson Battistella <tysonbattistella@gmail.com>
2018-05-18 14:56:41 -0700

    add author and subject to publication serializer

--- app/serializers/publication_serializer.rb
+++ app/serializers/publication_serializer.rb
@@ -1,3 +1,6 @@
 class PublicationSerializer < ActiveModel::Serializer
   attributes :arxiv_url, :pdf, :title, :abstract
+
+  has_many :authors
+  has_many :subjects
 end
---
+++ app/serializers/subject_serializer.rb
@@ -0,0 +1,3 @@
+class SubjectSerializer < ActiveModel::Serializer
+  attributes :key, :name
+end
